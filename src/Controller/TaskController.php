832952ac8a9a9e09e95fda5bdd1452acecf90ca9
mithridatem <mathieumithridate@adrar-formation.com>
2023-01-17 14:08:09 +0100

    ajout de la fonction showTaskByUtilName

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Task;
 use App\Repository\TaskRepository;
+use App\Entity\Util;
+use App\Repository\UtilRepository;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Doctrine\ORM\EntityManagerInterface;
@@ -70,4 +72,38 @@ class TaskController extends AbstractController
             //(tableau de donnée, code retour, entête http, groupe pour filtrer)
         }
     }
+
+    //fonction qui retourne en json une tache par son nom
+    #[Route('/task/utilname/{value}', name: 'app_task_utilname', methods: 'GET')]
+    public function showTaskByUtilName(TaskRepository $taskRepo,
+    UtilRepository $utilRepo, NormalizerInterface $normalizer, $value): Response
+    {
+        //stocker dans une variable l'objet utilisateur (BDD)
+        $user = $utilRepo->findOneBy(['name'=>$value]);
+        //stocker dans une variable un tableau d'objet task (BDD)
+        $tasks = $taskRepo->findBy(['util'=>$user]);
+        //test si aucune tache à été trouvé
+        if($tasks == null){
+            return $this->json(['error'=>'Aucune tache n\a été trouvé'],200,
+            ['Content-Type'=>'application/json','Access-Control-Allow-Origin'=> '*',
+            'Access-Control-Allow-Methods'=>'GET']);
+        }
+        //test si l'utilisateur n'existe pas
+        else if($user == null){
+            return $this->json(['error'=>'L\'utilisateur '.$value.' n\'existe pas'],200,
+            ['Content-Type'=>'application/json','Access-Control-Allow-Origin'=> '*',
+            'Access-Control-Allow-Methods'=>'GET']);
+        }
+        //sinon reourne le json de toutes les taches qui correspondent (qui sont ratachées à l'utilisateur)
+        else{
+            return $this->json($tasks,200,
+            ['Content-Type'=>'application/json','Access-Control-Allow-Origin'=> '*',
+            'Access-Control-Allow-Methods'=>'GET'],
+            ['groups'=>'tasks']);
+            //(tableau de donnée, code retour, entête http, groupe pour filtrer)
+        }
+    }
+
+
+
 }
